df4ca1cb8e6ad3d004f0c5f30fb3c60a12898637
nk98nk <nagatami@cy-com.co.jp>
2025-06-12 13:48:43 +0900

    ホーム

--- music-management/src/main/java/com/example/music_management/security/SecurityConfig.java
+++ music-management/src/main/java/com/example/music_management/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(request -> request
-                .requestMatchers("/register","/login","/error","/img/**","/css").permitAll()
+                .requestMatchers("/register", "/login", "/error", "/css/**", "/img/**").permitAll()
                 .anyRequest().authenticated())
             .formLogin(login -> login
                 .loginProcessingUrl("/login")
